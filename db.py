6d2317153a2c1325397786cb5eaad087af3988c2
DON62698 <sniper9540@gmail.com>
2025-08-01 23:30:42 +0900

    Update db.py

--- db.py
+++ db.py
@@ -9,12 +9,15 @@ def init_data():
         df = pd.DataFrame(columns=["date", "employee", "method"])
         df.to_csv(CSV_FILE, index=False)
 
-def add_record(employee, method, count=1):
-    date_today = datetime.today().strftime("%Y-%m-%d")
-    new_rows = pd.DataFrame([{"date": date_today, "employee": employee, "method": method}] * count)
+def add_record(employee, method, count=1, date_str=None):
+    if date_str is None:
+        date_str = datetime.today().strftime("%Y-%m-%d")
+
+    new_rows = pd.DataFrame([{"date": date_str, "employee": employee, "method": method}] * count)
     df = pd.read_csv(CSV_FILE)
     df = pd.concat([df, new_rows], ignore_index=True)
     df.to_csv(CSV_FILE, index=False)
+
 
 def get_all_records():
     if not os.path.exists(CSV_FILE):
